884d82c0c8536fdcccf2489a1a7530f796fa8f46
Tim Baxter <tim.baxter@cerner.com>
2014-05-21 09:08:38 -0500

    update for 0.4

--- CHANGES.md
+++ CHANGES.md
@@ -1,5 +1,8 @@
 # Change log
 
+## 0.4 
+* Fairly major cleanup and refactor, particularly templates.
+
 ## 0.3.1
 * Cleaned up some formatting
 * Hid "add event" from unauthenticated users
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages
 
 setup(
     name='tango-happenings',
-    version='0.3.2',
+    version='0.4',
     author=u'Tim Baxter',
     author_email='mail.baxter@gmail.com',
     url='https://github.com/tBaxter/django-happenings',
