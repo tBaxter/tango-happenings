82a9ceb04435107d573779f6778debeaafdbea62
Tim Baxter <tim.baxter@cerner.com>
2014-10-31 11:29:45 -0500

    put template apps back

--- test_settings.py
+++ test_settings.py
@@ -3,10 +3,12 @@ SECRET_KEY = "lorem ipsum"
 INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
+    'django.contrib.humanize',
     'django.contrib.sites',
 
     'tango_shared',
     'happenings',
+    'typogrify' # installed by shared, keeps templates happy
 )
 
 DATABASES = {
