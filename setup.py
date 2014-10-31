c54cbd7fbc06f7e2032972f8eae35dedd7798f54
Tim Baxter <tim.baxter@cerner.com>
2014-10-31 09:57:42 -0500

    testing against py3

--- setup.py
+++ setup.py
@@ -28,5 +28,7 @@ setup(
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python",
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
     ],
 )
