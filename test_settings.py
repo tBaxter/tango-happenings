1ab5aad14501e50bafc794bd4d253d8beb1d70c7
Tim Baxter <tim.baxter@cerner.com>
2014-10-31 08:47:11 -0500

    basic middleware for tests

--- test_settings.py
+++ test_settings.py
@@ -5,7 +5,7 @@ INSTALLED_APPS = (
     'django.contrib.contenttypes',
     'tango_shared',
     'happenings',
-    'typogrify' # installed by shared
+    'typogrify' # installed by shared, keeps templates happy
 )
 
 DATABASES = {
@@ -18,3 +18,10 @@ DATABASES = {
 ROOT_URLCONF = 'happenings.urls'
 SITE_ID = 1
 
+#stripped down middleware
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+)
